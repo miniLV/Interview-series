67bfa6d59e839430f44762e553c391abfac1221f
梁宇航 <liangyuhangApple@gmail.com>
2017-09-29 23:10:34 +0800

    initFile

---
+++ ButtonRotateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ButtonRotateDemo
+//
+//  Created by 梁宇航 on 2017/9/29.
+//  Copyright © 2017年 梁宇航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
